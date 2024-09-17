d77cc610dc0ff15fb10bbcb833c62721c9de4fbc
Sreypovprak <sreypovprak88@gmail.com>
2024-09-17 17:38:11 +0700

    add(31-mailService): config client url for cors.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Global Middleware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(helmet());
 
